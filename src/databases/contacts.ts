7bf262a05304fc52df4c1650e8890c688a58ad42
DevAX1T <59183076+DevAX1T@users.noreply.github.com>
2024-03-03 14:28:40 -0800

    Update contacts

--- src/databases/contacts.ts
+++ src/databases/contacts.ts
@@ -6,7 +6,7 @@ export default {
         /** HR + DC */
         HR_DC: 'Human Resources and Department Command',
         /** MP + MIS */
-        MP_MIS: 'MP and MIS.',
+        MP_MIS: 'MP and MIS',
         MP_MPI: 'MP and MPI',
         /** AOD * HC */
         AOD_HC: 'the Administrative Operations Director or High Command',
