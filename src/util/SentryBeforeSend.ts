3d252e8235a52488b707f5fc398472ead1ce1700
DevAX1T <59183076+DevAX1T@users.noreply.github.com>
2024-03-03 17:42:16 -0800

    Update error logging

--- src/util/SentryBeforeSend.ts
+++ src/util/SentryBeforeSend.ts
@@ -2,24 +2,14 @@ import { Event, EventHint } from '@sentry/node';
 import Global, { ILog } from '../Global';
 import Logger from './Logger';
 
-export default (event: Event, _: EventHint): Event | null => {
+export default (event: Event, hint: EventHint): Event | null => {
     let toReturn: any = event;
     try {
-        if (event.exception && event.exception.values) {
-            event.exception.values.forEach(exception => {
-                if (exception.value && exception.value === 'read ECONNRESET') return toReturn = null;
-                if (true) {// Tempfix because of sentry block !Global.isProd) {
-                    if (!exception.stacktrace?.frames) return;
-                    let msg = '';
-                    exception.stacktrace.frames.reverse().forEach(f => {
-                        try {
-                            msg += `\n at ${f.function} (${f.filename}:${f.lineno}:${f.colno})`;
-                        } catch (e) {}
-                    });
-                    Logger.log(ILog.Level.Error, 'Core', `Sentry error: ${exception.value}${msg}`);
-                    console.error(exception.value, msg);
-                }
-            });
+        const error = hint.originalException as Error;
+        if (error.message === 'read ECONNRESET') return toReturn = null;
+        if (Global.debugLevel === ILog.Level.DebugExtended || !Global.isProd) {
+            Logger.log(ILog.Level.Error, 'Core', `Sentry error: ${error}`);
+            console.log(error);
         }
     } catch {};
     return toReturn;
